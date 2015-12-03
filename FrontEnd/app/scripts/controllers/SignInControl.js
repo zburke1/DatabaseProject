09698ef980c1666c637b21528392951c04c78f99
Zac <zburke1@ufl.edu>
2015-12-02 19:51:39 -0500

    Added error checking to signin page

--- FrontEnd/app/scripts/controllers/SignInControl.js
+++ FrontEnd/app/scripts/controllers/SignInControl.js
@@ -7,7 +7,7 @@
  * # MainCtrl
  * Controller of the frontEndApp
  */
-angular.module('frontEndApp').controller('SignInControl', function ($scope,$http,$location,$route,$window,sessionService) {
+angular.module('frontEndApp').controller('SignInControl', function ($scope,$http,$location,$route,$window,$mdToast,sessionService) {
 	if(sessionService.get('uid')!=null){
 		$location.path('/');
 	}
@@ -15,6 +15,12 @@ angular.module('frontEndApp').controller('SignInControl', function ($scope,$http
 	$scope.SignUpRedirect = function(){
 		$location.path('/signup');
 	}
+	
+    $scope.openToast = function($event) {
+       $mdToast.show($mdToast.simple().textContent('Incorrect user/password').position('top left')
+           .hideDelay(1000));
+       // Could also do $mdToast.showSimple('Hello');
+     };
 
 	/* Check whether the HTTP Request is successful or not. */
 	$scope.signInPost = function(){
@@ -24,11 +30,16 @@ angular.module('frontEndApp').controller('SignInControl', function ($scope,$http
 		
 	$.post("http://127.0.0.1:8010/signInPost.php", {userId: userID, password: userPass}).done(function(data) {
 		console.log("Finished");
-		sessionService.set('uid',data[0].userId);
-		sessionService.set('name',data[0].name);
-		sessionService.set('staff',data[0].is_staff);
-		var landingUrl = "http://localhost:9000/"; //URL complete
-		$window.location.href = landingUrl;
+		if(data[0] != undefined){
+			sessionService.set('uid',data[0].userId);
+			sessionService.set('name',data[0].name);
+			sessionService.set('staff',data[0].is_staff);
+			var landingUrl = "http://localhost:9000/"; //URL complete
+			$window.location.href = landingUrl;
+		}
+		else{
+			$scope.openToast();
+		}
   });
 }
   });
